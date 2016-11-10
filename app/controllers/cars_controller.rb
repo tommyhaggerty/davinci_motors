1fba7280d6acad57d20542edd62d11ea7ffdfb5e
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-11-09 18:23:20 -0700

    DaVinci Motors coded
    * Added a before action to set the car
    * Reworked the code to properly set @cars
    * Added method to ‘show, edit, and update’ cars
    * Updated the create method
    * Added a destroy method
    * Added a set_car method to private to be called 
      in the before action.
    
    * Fixed a broken car.rb model by inheriting from
      ApplicationRecord
    * Added views for index, edit, and show
    * Wrote failing tests in creating_cars_spec.rb. 
    
    [#133235323]

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,15 +1,52 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update ]
+
   def index
+    @cars = Car.all
+  end
+
+  def show
   end
 
   def new
     @car = Car.new
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @car.update(car_params)
+        format.html { redirect_to @car, notice: "#{@car.year} #{@car.make} #{@car.model} updated" }
+        format.json { render :show, status: :ok, location: @car }
+      else
+        format.html { render :edit }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def create
-    car = Car.new(car_params)
-    redirect_to root_path,
-      notice: "#{car.year} #{car.make} #{car.model} created"
+    @car = Car.new(car_params)
+
+    respond_to do |format|
+      if @car.save
+        format.html { redirect_to root_path, notice: "#{@car.year} #{@car.make} #{@car.model} created" }
+        format.json { render :show, status: :created, location: @car }
+      else
+        format.html { render :new }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def set_car
+    @car = Car.find(params[:id])
   end
 
   def car_params
--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -15,4 +15,60 @@ feature 'Creating Cars' do
 
     expect(page).to have_content('1967 Ford Mustang created')
   end
+
+  scenario 'can create a second car' do
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Mustang'
+    fill_in 'Year', with: '1967'
+    fill_in 'Price', with: '2300'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('1967 Ford Mustang created')
+
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Prius'
+    fill_in 'Year', with: '2013'
+    fill_in 'Price', with: '17500'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('2013 Toyota Prius created')
+  end
+
+  scenario 'User can edit a car' do
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Prius'
+    fill_in 'Year', with: '2013'
+    fill_in 'Price', with: '17500'
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('2013 Toyota Prius created')
+
+    visit '/'
+
+    click_link 'Edit'
+
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Prius'
+    fill_in 'Year', with: '2013'
+    fill_in 'Price', with: '17800'
+
+    click_button 'Update Car'
+
+    expect(page).to have_content ('2013 Toyota Prius updated')
+  end
 end
